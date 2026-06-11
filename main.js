6d69ff37fab1257ad1af2c1628a62f8d27ddd6cd
cochanae-del <co.chanae@gmail.com>
2026-06-11 23:27:29 +0000

    Create a function to search for a word

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // Write your code below
+var words = [ "Apple", "Berry", "Cherry", "Banana"]
+function findElement(words,word) {
+   for (var i = 0; i < words.length; i = i + 1); {
+
+        if (words[i] === word)
+        return true;
+}  
+        return false;
+}  
+   
+       console.log(findElement(words, "Orange"));
+       
