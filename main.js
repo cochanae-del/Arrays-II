647d560bd8c42e5b59e1a4381869099b6f3bdaed
cochanae-del <co.chanae@gmail.com>
2026-06-12 03:35:26 +0000

    Create a function that prints boolean if word is found in array.

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 // Write your code below
 var words = ["Apple", "Berry", "Cherry", "Banana"]
+var word = "Berry";
 
-function findElement(words, word) {
-   for (var i = 0; i < words.length; i++); {
+function findElement(words, word) { 
+   for (var i = 0; i < words.length; i ++) {
 
-        if (words[i] === word)
-        return true;
-}  
+        if (words[i] == word)  {
+        return true;  }
+} 
+{
         return false;
-}  
+}
+}
+console.log(findElement(words, word));
+  
+
+
+
+
+
+// var dogs =  ["pomeranian", "corgi", "shih tzu"];
+            
+// var searchFor = "corgi";
+
+// for (var i = 0; i < dogs.length; i++) { 
+//     if (dogs[i] === searchFor) {
+//         console.log("Found the dog breed in this list!");
+//     }
+// }
+
+// // 
+ 
    
-       console.log(findElement("Cherry"));
+       
